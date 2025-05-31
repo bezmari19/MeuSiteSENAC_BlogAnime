1b06e8afd66e744beefcda523229fcbe1795bf92
bezmari19 <marina.bezerraf19@gmail.com>
2025-05-31 00:33:40 -0300

    commit java e html

--- javaScript.js
+++ javaScript.js
@@ -1,9 +1,77 @@
-const toggleThemeButton = document.getElementById("toggle-theme");
-const body = document.body;
+let currentLanguage = localStorage.getItem("language") || "pt";
 
-function toggleTheme() {
-  body.classList.toggle("dark-mode");
-  toggleThemeButton.textContent = body.classList.contains("dark-mode") ? "Modo Claro" : "Modo Escuro";
+const translations = {
+  pt: {
+      LightTheme: "claro",
+      DarkTheme: "escuro",
+  },
+  en: {
+      LightTheme: "light",
+      DarkTheme: "dark",
+  }
+};
+
+function toggleLanguage() {
+  currentLanguage = currentLanguage === "pt" ? "en" : "pt";
+  localStorage.setItem("language", currentLanguage);
+  updateLanguage();
 }
 
-toggleThemeButton.addEventListener("click", toggleTheme);
+function updateLanguage() {
+  const elements = document.querySelectorAll("[data-pt][data-en]");
+  elements.forEach(element => {
+      const text = element.getAttribute(`data-${currentLanguage}`);
+      if (text) {
+          if (element.innerHTML.includes("&copy;")) {
+              element.innerHTML = text;
+          } else {
+              element.textContent = text;
+          }
+      }
+  });
+  
+  document.getElementById("current-lang").textContent = currentLanguage.toLocaleUpperCase() === "pt" ? "eng" : "pt";
+  
+  
+  const themeText = document.querySelector(".toggleTheme span");
+  const isDark = document.body.getAttribute("data-theme") === "dark";
+  const themeKey = isDark ? "theme_light" : "theme_dark";
+  themeText.textContent = translations[currentLanguage][themeKey];
+}
+
+
+function toggleTheme() {
+  const currentTheme = document.body.getAttribute("data-theme") || "light";
+  const newTheme = currentTheme === "dark" ? "light" : "dark";
+  
+  document.body.setAttribute("data-theme", newTheme);
+  localStorage.setItem("theme", newTheme);
+  
+  const themeIcon = document.getElementById("temaIcone");
+  const themeText = document.querySelector(".temaIcone span");
+  
+  if (newTheme === "dark") {
+      themeIcon.className = "lightningTheme";
+      themeText.textContent = translations[currentLanguage]["LightTheme"];
+  } else {
+      themeIcon.className = "darkerTheme";
+      themeText.textContent = translations[currentLanguage]["DarkTheme"];
+  }
+}
+
+
+function initTheme() {
+  const savedTheme = localStorage.getItem("theme") || "light";
+  document.body.setAttribute("data-theme", savedTheme);
+  
+  const themeIcon = document.getElementById("temaIcone");
+  const themeText = document.querySelector(".temaIcone span");
+  
+  if (savedTheme === "dark") {
+      themeIcon.className = "lightningTheme";
+      themeText.textContent = translations[currentLanguage]["LightTheme"];
+  } else {
+      themeIcon.className = "darkerTheme";
+      themeText.textContent = translations[currentLanguage]["DarkTheme"];
+  }
+}
